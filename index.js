3142e84def967cc000c6decca189ac8df86a6d31
Sz-F <82759648+Sz-F@users.noreply.github.com>
2022-01-25 17:50:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("txt");
+/*    <script src="scripts/index.js"></script>*/
+
+/*
+.gear__text {
+    width: 100%;
+    color: #2f2f2f;
+    font-family: "Hind", "Arial", sans-serif;
+    font-size: 0.8rem;
+    font-weight: 600;
+    line-height: 1.5;
+    padding: 0.2rem 0.5rem;
+    */
